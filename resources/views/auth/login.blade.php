1329c7f9fc43f53afebcf3063c866ac96a693c0f
Jere-killer <gm_cardona@hotmail.com>
2020-10-19 17:30:53 -0600

    boton aceptar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -79,7 +79,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i> Acceder
+                                    <i class="fa fa-btn fa-sign-in"></i> Aceptar
                                 </button>
 
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Olvidaste tu password?</a>
